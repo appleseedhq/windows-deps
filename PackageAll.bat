600c1534a95026e2967331f4ff7cf5ee6cb18c23
François Beaune <francois.beaune@allegorithmic.com>
2018-09-01 14:37:09 +0200

    Update packaging script

--- PackageAll.bat
+++ PackageAll.bat
@@ -32,3 +32,4 @@ set zip="c:\Program Files\7-Zip\7z.exe"
 %zip% a appleseed-deps-stage-win64-vs110-vN.zip stage\vc11
 %zip% a appleseed-deps-stage-win64-vs120-vN.zip stage\vc12
 %zip% a appleseed-deps-stage-win64-vs140-vN.zip stage\vc14
+%zip% a appleseed-deps-stage-win64-vs141-vN.zip stage\vc141
